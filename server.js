9af5c013cd5af787d01eb06e5997b8215f1bdc07
Vitaliya Gubanova <vitaliagubanova@gmail.com>
2025-05-29 02:10:49 +0300

    123456

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+// Объявление констант (обратите внимание на регистр)
+const HOST = 'localhost';
+const PORT = 8000;
+
+const handler = (req, res) => {
+    res.writeHead(200);
+    res.end('JavaScript - классный язык программирования!');
+};
+
+const server = http.createServer(handler);
+server.listen(PORT, HOST, () => {
+    console.log(`Сервер запущен по адресу http://${HOST}:${PORT}`);
+});

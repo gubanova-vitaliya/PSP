01f849f47b366400ca22d29e435af75c9553fe64
Vitaliya Gubanova <vitaliagubanova@gmail.com>
2025-05-31 20:42:02 +0300

    final

--- example-nestjs/src/main.ts
+++ example-nestjs/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(resolve(__dirname, '..', 'public'));
   // Включите CORS с настройками
   app.enableCors({
     origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // Разрешенные домены
@@ -11,7 +13,8 @@ async function bootstrap() {
     credentials: true,
   });
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+

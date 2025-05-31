074cc933291a8e867e45bd3f1e1aaa2eb7a7d98f
Vitaliya Gubanova <vitaliagubanova@gmail.com>
2025-05-31 10:30:41 +0300

    1111111

--- example-nestjs/src/main.ts
+++ example-nestjs/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Включите CORS с настройками
+  app.enableCors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // Разрешенные домены
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
